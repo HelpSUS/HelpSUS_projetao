00a343e14d2356ba870e1051f2a7165b8b6181da
Lucas Venancio <lucas.venancio@outlook.com>
2023-12-05 18:02:32 -0300

    inicio do setup do front
    Iniciei com react e styled components pra estilização

---
+++ projetao/src/App.js
@@ -0,0 +1,13 @@
+import { Maincomponent } from "./styles";
+
+function App() {
+  return (
+    <div>
+      <Maincomponent>
+        <h1>Projetão</h1>
+      </Maincomponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ projetao/src/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Maincomponent = styled.main`
+    h1{
+        color: black;
+    }
+`;
